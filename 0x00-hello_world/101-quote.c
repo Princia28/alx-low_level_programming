cbf11f772c41142a69e223b4431dedd075b47dbe
Princia28 <111355382+Princia28@users.noreply.github.com>
2022-09-08 14:45:57 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 #include <string.h>
 #include <unistd.h>
 /**
- * main - main block
+ * main - Print quote
  *
  * Return: 1
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-	
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n",s);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
