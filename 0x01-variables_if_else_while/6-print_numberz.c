aa05e5b194ac44440520f7d7200c3c08a465d1c3
Princia28 <111355382+Princia28@users.noreply.github.com>
2022-09-10 13:51:36 -0700

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,47 +1,20 @@
 #include <stdlib.h>
-
 #include <stdio.h>
-
 #include <time.h>
 
-
-
 /**
+ * main - Prints the numbers since 0 to 9
  *
- *  * main - Prints base 10 numbers
- *
- *   * @void: Empty parameter list for main.
- *
- *    *
- *
- *     * Description: Prints numbers 0-9
- *
- *      *
- *
- *       * Return: 0 for success
- *
- *       */
-
+ * Return: 0 for success
+ */
 int main(void)
-
 {
-
-		int num = 48;
-
-
-
-			while (num <= 57)
-
-					{
-
-								putchar(num);
-
-										num++;
-
-											}
-
-				putchar('\n');
-
-					return (0);
-
+int num = 48;
+while (num <= 57)
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
 }
