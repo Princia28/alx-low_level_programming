7ae7d4ee1d154790322a5ed24dff93666b97be93
Princia28 <111355382+Princia28@users.noreply.github.com>
2022-09-10 14:08:26 -0700

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,63 +1,26 @@
 #include <stdlib.h>
-
 #include <stdio.h>
-
 #include <time.h>
 
-
-
 /**
+ * main - Prints base 16
  *
- *  * main - Prints base 16
- *
- *   * @void: Empty parameter list for main.
- *
- *    *
- *
- *     * Description: Prints all hexadecimals
- *
- *      *
- *
- *       * Return: 0 for success
- *
- *       */
-
+ * Return: 0 for success
+ */
 int main(void)
-
 {
-
-		int num = 48;
-
-
-
-			while (num <= 57)
-
-					{
-
-								putchar(num);
-
-										num++;
-
-											}
-
-
-
-				num = 97;
-
-
-
-					while (num <= 102)
-
-							{
-
-										putchar(num);
-
-												num++;
-
-													}
-
-						putchar('\n');
-
-							return (0);
-
+int num = 48;
+while (num <= 57)
+{
+putchar(num);
+num++;
+}
+num = 97;
+while (num <= 102)
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
 }
