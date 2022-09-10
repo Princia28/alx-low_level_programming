bc35ffa26a64b481b585eaf7b3160619d1cd416f
Princia28 <111355382+Princia28@users.noreply.github.com>
2022-09-10 11:59:21 -0700

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	int n;
 	srand(time(0));
+	
 	n = rand() - RAND_MAX / 2;
 	
 	if (n > 0)
@@ -22,8 +23,8 @@ int main(void)
 	}
 	else
 	{
-	printf("%d is negative\n", n);
+
+printf("%d is negative\n", n);
 	}	
-	
         return (0);
 }
