0f1d14c2a1662d09369cbee7637226c08787f75c
Princia28 <111355382+Princia28@users.noreply.github.com>
2022-09-10 13:41:26 -0700

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,47 +1,20 @@
 #include <stdlib.h>
-
 #include <stdio.h>
-
 #include <time.h>
 
-
-
 /**
+ * main - Prints the numbers since 0 to 9
  *
- *  * main - Prints base 10 numbers
- *
- *   * @void: Empty parameter list for main.
- *
- *    *
- *
- *     * Description: Prints numbers 0-9
- *
- *      *
- *
- *       * Return: 0 for success
- *
- *       */
-
+ * Return: 0 for success
+ */
 int main(void)
-
 {
-
-		char num = '0';
-
-
-
-			while (num <= '9')
-
-					{
-
-								putchar(num);
-
-										num++;
-
-											}
-
-				putchar('\n');
-
-					return (0);
-
+char num = '0';
+while (num <= '9')
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
 }
