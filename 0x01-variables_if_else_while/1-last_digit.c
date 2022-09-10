ae35247b64187f0e23c2fb144dc958db558b2756
Princia28 <111355382+Princia28@users.noreply.github.com>
2022-09-10 12:59:19 -0700

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,79 +1,25 @@
-#include <stdlib.h>
-
 #include <stdio.h>
-
+#include <stdlib.h>
 #include <time.h>
 
-
-
 /**
+ * main - print last digit in random number
  *
- *  * main - Positive or Negative - print sign of random number
- *
- *   * @void: Empty parameter list for main.
- *
- *    *
- *
- *     * Description: this if/else statement delares the sign (positive
- *
- *      * or negative) of a random number. It also states if the value
- *
- *       * is zero.
- *
- *        *
- *
- *         * Return: 0 for success
- *
- *         */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
+int n, last;
 
-	  /**
-	   *
-	   *    * variable n has been declared
-	   *
-	   *       * coding then assigns the random value.
-	   *
-	   *          */
-
-		int n;
-
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-
-
-					if (n > 0)
-
-							{
-
-										printf("%d is positive\n", n);
-
-											}
-
-						else if (n == 0)
-
-								{
-
-											printf("%d is zero\n", n);
-
-												}
-
-							else
-
-									{
-
-												printf("%d is negative\n", n);
-
-													}
-
-
-
-								return (0);
-
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last = n % 10;
+if (last > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
+else if (last == 0)
+printf("Last digit of %d is %d and is 0\n", n, last);
+else if (last < 6 && last != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+return (0);
 }
