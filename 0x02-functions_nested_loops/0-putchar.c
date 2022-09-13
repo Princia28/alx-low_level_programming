d5fc0df96a1c40d9ed92aa4afb1d13c2d0053ac9
Princia28 <111355382+Princia28@users.noreply.github.com>
2022-09-13 15:48:27 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * main - function to print
+ * main - Entry point
  *
- * Return: 0
- */
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-unsigned int c;
-for (c = 0; c < sizeof(holberton); c++)
-{
-_putchar(holberton[c]);
-}
+char str[] = "_putchar";
+int ch;
+for (ch = 0; ch < 8; ++ch)
+_putchar(str[ch]);
 _putchar('\n');
 return (0);
 }
