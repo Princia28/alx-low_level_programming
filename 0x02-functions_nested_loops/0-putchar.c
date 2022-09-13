a29984144fad0e2fc4db62f083d28553eeccdab3
Princia28 <111355382+Princia28@users.noreply.github.com>
2022-09-13 15:27:24 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,45 +1,18 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ * main - prints _putchar
  *
- *  * main - prints Holberton
- *
- *   *
- *
- *    * Description: prints Holberton
- *
- *     *
- *
- *      * Return: 0 (Success)
- *
- *       */
-
-
-
+ * Return: 0 (Success)
+ */
 int main(void)
-
 {
-
-		char text[10] = "Holberton";
-
-			int i = 0;
-
-
-
-				for (i = 0; i < 9; i++)
-
-						{
-
-									_putchar(text[i]);
-
-										}
-
-					_putchar('\n');
-
-
-
-						return (0);
-
+char text[10] = "Holberton";
+int i = 0;
+for (i = 0; i < 9; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+return (0);
 }
