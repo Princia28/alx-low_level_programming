4335d769a886c4c14996cf5ee28ca9d2501b3a56
Princia28 <111355382+Princia28@users.noreply.github.com>
2022-09-13 23:24:21 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,41 +1,18 @@
 #include"main.h"
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: prints _putchar using putchar prototype
  *
- *   *
- *
- *    * Description: prints _putchar using putchar prototype
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *      */
-
-
-
+ * Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-
-		char str[] = "_putchar";
-
-			int ch;
-
-
-
-				for (ch = 0; ch < 8; ++ch)
-
-							_putchar(str[ch]);
-
-					_putchar('\n');
-
-
-
-						return (0);
-
+char str[] = "_putchar";
+int ch;
+for (ch = 0; ch < 8; ++ch)
+_putchar(str[ch]);
+_putchar('\n');
+return (0);
 }
