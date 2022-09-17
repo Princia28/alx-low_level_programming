7903c38d9a329be6ba328e1606a1c0717e3872c1
Princia28 <111355382+Princia28@users.noreply.github.com>
2022-09-17 09:53:06 -0700

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,17 +2,17 @@
 
 main()
 {
-	int i = 0 , j;
-	while(i<5)
-	{
-		j = 0;
-		while(j<3)
-		{
-			printf("2 ");
-			j++;
-		}
-		printf("\n");
-		i++;
-	}
-	return 0;
+int i = 0 , j;
+while(i<5)
+{
+j = 0;
+while(j<3)
+{
+printf("2 ");
+j++;
+}
+printf("\n");
+i++;
+}
+return 0;
 }
