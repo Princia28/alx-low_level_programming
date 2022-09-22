5db5c489794a874de280df79e680695d8a9f231e
Princia28 <princiamutoni45@gmail.com>
2022-09-22 13:45:50 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * print_number - prints an integer
 * @n: integer to be printed
@@ -18,5 +19,4 @@ void print_number(int n)
 		print_number(num / 10);
 
 	_putchar((num % 10) + '0');
-	return (0);
 }
