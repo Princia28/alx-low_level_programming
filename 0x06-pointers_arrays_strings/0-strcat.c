d46f35ad2fadfc7711889bd798fa0bcf8f0b702e
Princia28 <111355382+Princia28@users.noreply.github.com>
2022-09-22 07:44:06 -0700

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,6 +2,7 @@
  * _strcat - function that concatenates two strings.
  * @dest: pointer to destination input
  * @src: pointer to source input
+ *
  * Return: pointer to resulting string @dest
 */
 
